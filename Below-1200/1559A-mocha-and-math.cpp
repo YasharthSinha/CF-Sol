6d646342e4f23bf67ce64a9acf4f7b0d2e35690f
YasharthSinha <100145488+YasharthSinha@users.noreply.github.com>
2025-02-27 02:57:29 +0530

    Create 1559A-mocha-and-math.cpp

---
+++ Below-1200/1559A-mocha-and-math.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+#define ll                    long long int
+#define pb                    push_back
+#define F                     first
+#define S                     second
+#define mp                    make_pair
+#define MOD                   1000000007
+#define vi                    vector<int>
+#define vll                   vector<ll>
+#define pll                   pair<ll,ll>
+#define pii                   pair<int,int>
+#define all(arr)              arr.begin(),arr.end()
+#define mid(l,h)              (l+(h-l)/2)
+#define eb                    emplace_back
+#define ull                   unsigned long long
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n);
+        for(int x=0;x<n;x++) cin>>arr[x];
+        ll ans=arr[0];
+        for(int x=1;x<n;x++) ans&=arr[x];
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
