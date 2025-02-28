8a1fccf43ea184044d231cc7537aedf4a2dfcbfb
YasharthSinha <100145488+YasharthSinha@users.noreply.github.com>
2025-03-01 04:28:13 +0530

    Create 1339A-filling-diamonds.cpp

---
+++ Below-1200/1339A-filling-diamonds.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define ll                    long long int
+#define vi                    vector<int>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n<<"\n";
+    }
+    return 0;
+}
